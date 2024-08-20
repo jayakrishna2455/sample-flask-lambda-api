8448f2843db234fd167a64eedd75658bcb8ce97f
jayakrishna2455 <160151358+jayakrishna2455@users.noreply.github.com>
2024-08-20 10:56:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, request, redirect, Response
 from gallery import gallery
 import base64
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://5a8a6238ee1a1c78eec134945dfa20fb@o4507796010958848.ingest.us.sentry.io/4507807988252672",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 app = Flask(__name__)
 # Todo: Update this to a bucket in your account
